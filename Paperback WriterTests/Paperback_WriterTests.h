df0d1d4ba68a62d3954509409ee12c62d3b986ac
Troy HARRIS <troy.harris@Troys-MacBook-Air.local>
2013-05-09 18:09:48 -0700

    Initial Commit

---
+++ Paperback WriterTests/Paperback_WriterTests.h
@@ -0,0 +1,13 @@
+//
+//  Paperback_WriterTests.h
+//  Paperback WriterTests
+//
+//  Created by Troy HARRIS on 5/9/13.
+//  Copyright (c) 2013 Lone Yeti. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Paperback_WriterTests : SenTestCase
+
+@end
---
+++ Paperback WriterTests/Paperback_WriterTests.m
@@ -0,0 +1,32 @@
+//
+//  Paperback_WriterTests.m
+//  Paperback WriterTests
+//
+//  Created by Troy HARRIS on 5/9/13.
+//  Copyright (c) 2013 Lone Yeti. All rights reserved.
+//
+
+#import "Paperback_WriterTests.h"
+
+@implementation Paperback_WriterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Paperback WriterTests");
+}
+
+@end
