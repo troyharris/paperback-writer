07eed3a9e4ee703a2750d6eafdf93543edeacc72
Troy HARRIS <troy.harris@djt-tech.com>
2013-07-28 08:34:11 -0700

    Added character tab view controller

---
+++ Paperback Writer/IPadCharacterRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  iPadCharacterRootViewController.h
+//  Paperback Writer
+//
+//  Created by Troy HARRIS on 7/27/13.
+//  Copyright (c) 2013 Lone Yeti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iPadCharacterRootViewController : UIViewController
+
+@end
---
+++ Paperback Writer/IPadCharacterRootViewController.m
@@ -0,0 +1,38 @@
+//
+//  iPadCharacterRootViewController.m
+//  Paperback Writer
+//
+//  Created by Troy HARRIS on 7/27/13.
+//  Copyright (c) 2013 Lone Yeti. All rights reserved.
+//
+
+#import "iPadCharacterRootViewController.h"
+
+@interface iPadCharacterRootViewController ()
+
+@end
+
+@implementation iPadCharacterRootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
